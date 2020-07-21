bdf592cb5f8ce8cc50f8e5d9fa3f10ca4a7917de
Daniel <danmoran.pro@gmail.com>
2020-07-20 23:20:07 -0600

    Updates

---
+++ precache-manifest.534ee44d9043db041c3512d31b362c2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f275fd08e7bdc6cabb78",
+    "url": "/me/static/css/main.07f1d311.chunk.css"
+  },
+  {
+    "revision": "f275fd08e7bdc6cabb78",
+    "url": "/me/static/js/main.f275fd08.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d8dd349d4c026f33ba3131cbdb80ef9a",
+    "url": "/me/index.html"
+  }
+];
